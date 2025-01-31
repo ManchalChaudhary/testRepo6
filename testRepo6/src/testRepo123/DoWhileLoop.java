37efbda4460e1a71bf8f7bc3e8fddaf2923fc5d5
manchal <manchalc@gmail.com>
2025-01-30 21:07:23 -0600

    234

---
+++ testRepo6/src/testRepo123/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package testRepo123;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		int i = 20;
+		// do{}while()
+		
+		do {System.out.println(i); i++;} 
+		while (i < 15); 
+
+
+	}
+
+}
